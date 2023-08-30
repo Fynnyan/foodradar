2e34de5d6752d67263334341068aae412a5dca35
Fynnian <you@example.com>
2023-08-30 21:53:25 +0200

    simple frontend with gradle config

---
+++ frontend/build.gradle.kts
@@ -0,0 +1,18 @@
+import com.github.gradle.node.npm.task.NpmTask
+
+plugins {
+    id("com.github.node-gradle.node") version "7.0.0"
+}
+
+val npmStart by tasks.creating(NpmTask::class) {
+    args = listOf("run", "start")
+}
+
+val npmBuild by tasks.creating(NpmTask::class) {
+    args = listOf("run", "build")
+}
+
+node {
+    download = false
+    version = "18.15.0"
+}
