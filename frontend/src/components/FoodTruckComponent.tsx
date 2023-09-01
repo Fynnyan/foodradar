e46ad047e56a906eed1c9edeea60f06b4227c876
Fynnian <you@example.com>
2023-09-01 19:26:03 +0200

    - display a message when there are no food truck entries

--- frontend/src/components/FoodTruckComponent.tsx
+++ frontend/src/components/FoodTruckComponent.tsx
@@ -1,7 +1,7 @@
 import {FoodTruck} from "./Data";
-import {Card, CardContent, CardHeader, Link, Typography} from "@mui/material";
+import {Box, Card, CardContent, CardHeader, Link, SvgIcon, Typography} from "@mui/material";
+import {ReactComponent as TruckGoneIcon} from '../icons/truck_gone.svg';
 import React from "react";
-
 
 interface FoodTruckComponentProps {
     foodTrucks: FoodTruck[]
@@ -9,7 +9,7 @@ interface FoodTruckComponentProps {
 
 export const FoodTruckComponent = (props: FoodTruckComponentProps) => {
     return <Card>
-        <CardHeader title={"Food Trucks Today"}/>
+        <CardHeader title={"Today's Food Trucks"}/>
         <CardContent>
             {props.foodTrucks.map((truck) =>
                 <>
@@ -17,6 +17,15 @@ export const FoodTruckComponent = (props: FoodTruckComponentProps) => {
                     <Link href={truck.web}>{truck.location}</Link>
                 </>
             )}
+            {props.foodTrucks.length === 0 ? <NoFoodTrucks/> : ""}
         </CardContent>
     </Card>
 }
+
+const NoFoodTrucks = () =>
+    <Box sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+        <SvgIcon viewBox={"0 0 36 36"} sx={{fontSize: "10rem"}}>
+            <TruckGoneIcon/>
+        </SvgIcon>
+        <Typography variant={"body1"}>There are no food trucks today</Typography>
+    </Box>
